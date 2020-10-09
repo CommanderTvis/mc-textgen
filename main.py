8d4646bffbfc353f3a82d5a937cc7133b12f3394
Iaroslav Postovalov <postovalovya@gmail.com>
2020-10-09 10:28:53 +0700

    Initial commit

---
+++ main.py
@@ -0,0 +1,15 @@
+from typing import List, Dict, Tuple
+
+_Dataset = Tuple[Dict[str, List[str]], List[str]]
+
+
+def _process_file(uri: str) -> List[str]:
+    pass
+
+
+def _build_map_and_initial_words(words: List[str]) -> _Dataset:
+    pass
+
+
+def _generate(n: int, dataset: _Dataset) -> str:
+    pass
